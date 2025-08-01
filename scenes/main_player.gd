8aa2bc368babab0b3d3cfa05611d57c43359efa0
Keerthi-Vijaibabu <keerthi.vijaibabu@gmail.com>
2025-08-01 07:47:45 +0530

    Main_player Sprite sheet added

--- scenes/main_player.gd
+++ scenes/main_player.gd
@@ -5,6 +5,8 @@ const ACCEL = 2.0
 
 var input: Vector2
 
+@onready var sprite = $AnimatedSprite2D
+
 func get_input():
 	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
@@ -18,4 +20,19 @@ func _process(delta):
 	
 	move_and_slide()
 		
+	update_animation(playerInput)
+
+func update_animation(direction: Vector2):
+	if direction == Vector2.ZERO:
+		sprite.stop()
+		return
+
+	if abs(direction.x) > abs(direction.y):
 		
+		sprite.animation = "right" if direction.x > 0 else "left"
+	else:
+		
+		sprite.animation = "front" if direction.y > 0 else "back"
+
+	sprite.play()
+
