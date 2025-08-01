2979f98fe8a66b9ddb9ecef40ee6ae315940abef
Keerthi-Vijaibabu <keerthi.vijaibabu@gmail.com>
2025-08-01 07:16:28 +0530

    Score label added

--- scenes/wood_cutting/ScoreLabel.gd
+++ scenes/wood_cutting/ScoreLabel.gd
@@ -1,6 +1,5 @@
 extends Label
 
 func _process(delta):
-	var score_manager = get_tree().get_root().get_node("Main/ScoreManager")
 	var ScoreManager =get_tree().get_root().get_node("WoodCutting/ScoreManager")
 	text = "Score: %d" % ScoreManager.score
