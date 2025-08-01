2aa0146edc4cde5d20ab411d3a33124f985b77c1
Kavyasree241005 <kavyasreesreevani@email.com>
2025-08-01 16:26:47 +0530

    Lumberjack

---
+++ scenes/lumberjack/lumberjack.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var spawn_object: PackedScene = preload("res://scenes/lumberjack/tree.tscn")
+@export var spawn_number = 40
+
+var random = RandomNumberGenerator.new()
+
+func _ready():
+	spawn()
+
+func get_random_position(screen_width: float) -> Vector2:
+	random.randomize()
+	var x = random.randf_range(-abs(screen_width / 2), abs(screen_width / 2))
+	var y = -2000
+	return Vector2(x, y)
+
+func spawn():
+	for i in range(spawn_number):
+		await get_tree().create_timer(1).timeout
+		var obj = spawn_object.instantiate()
+		obj.position = get_random_position(1024)
+		add_child(obj)
