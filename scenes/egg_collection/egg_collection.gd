8bc34c8e823921ee8f0148e575eca5fe4ffdd207
Keerthi-Vijaibabu <keerthi.vijaibabu@gmail.com>
2025-08-02 14:40:44 +0530

    Fish bug cleared

--- scenes/egg_collection/egg_collection.gd
+++ scenes/egg_collection/egg_collection.gd
@@ -2,15 +2,19 @@ extends Node2D
 
 @export var spawn_object: PackedScene = preload("res://scenes/egg_collection/egg.tscn")
 @export var level_duration_sec: float = 120.0  
-@export var spawn_number = 50
+@export var spawn_number = 120
 
 @onready var time_label: Label = $TimeLabel
 var remaining_time: float = 120.0
 @onready var warning_color: Color = Color.RED
 @onready var normal_color: Color = Color.WHITE
-@onready var audio_player = $Timer_Beeps 
+@onready var audio_player= $Timer_Beeps 
 @onready var original_font_scale: float = time_label.scale.x  
 var flashing: bool = false
+
+var missed_eggs: int = 0
+@onready var scoremanager: Node2D = $ScoreManager
+
 
 var random = RandomNumberGenerator.new()
 
@@ -29,14 +33,20 @@ func _process(delta):
 
 		# Flash in last 10 seconds
 		if remaining_time <= 10:
-			audio_player.play()
 			time_label.modulate = warning_color
 			flash_label()
 		else:
 			time_label.modulate = normal_color
+				
+		if scoremanager.score == 10:
+			await get_tree().create_timer(2).timeout 
+			on_level_complete()
+		
 	else:
 		remaining_time = 0
 		time_label.text = "00:00"
+		await get_tree().create_timer(2).timeout
+		on_level_complete()
 
 func flash_label():
 	var flash_speed = 8.0  # higher = faster flicker
@@ -66,4 +76,27 @@ func spawn():
 
 func on_level_complete():
 	print("Level Complete!")
-	get_tree().change_scene_to_file("res://scenes/egg_collection/level_complete_screen.tscn")
+	get_tree().change_scene_to_file("res://scenes/egg_collection/Game_over.tscn")
+
+
+func _on_timer_timeout() -> void:
+	audio_player.play()
+
+func on_egg_missed():
+	missed_eggs += 1
+	print("Missed eggs: %d" % missed_eggs)
+
+	if missed_eggs >= 3:
+		reset_score()
+
+func reset_score():
+	print("Missed 3 eggs! Score reset.")
+	scoremanager.score = 0
+	missed_eggs = 0 
+	
+
+func _on_miss_zone_body_entered(body: Node2D) -> void:
+	if body.is_in_group("egg"):
+		print("Missed")
+		on_egg_missed()
+		body.queue_free()
