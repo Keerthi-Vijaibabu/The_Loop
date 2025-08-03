1c6b1654687c74ad1af4b246981bf808a45fe000
Keerthi-Vijaibabu <keerthi.vijaibabu@gmail.com>
2025-08-03 17:35:12 +0530

    reduced the music volume in main

--- scenes/main_2.gd
+++ scenes/main_2.gd
@@ -1,19 +1,20 @@
 extends Node2D
 
-func _farm_body_entered(body: CharacterBody2D) -> void:
+func _on_farm_body_entered(body: CharacterBody2D) -> void:
 	if body.name == "CharacterBody2D":
 		call_deferred("change_scene_1")
 
 func change_scene_1():
-	get_tree().change_scene_to_file("res://scenes/egg_collection/")
-
-func _maze_body_entered(body: CharacterBody2D) -> void:
-	if body.name == "CharacterBody2D":
-		call_deferred("change_scene_2")
+	get_tree().change_scene_to_file("res://scenes/mm_egg_collection.tscn")
 
 func change_scene_2():
-	get_tree().change_scene_to_file("res://scenes/maze1.tscn")
+	get_tree().change_scene_to_file("res://scenes/mm_maze.tscn")
 	
 func check_progress():
 	if (GlobalSceneManager.gold == 10000):
 		get_tree().change_scene_to_file("res://scenes/main2.tscn")
+
+
+func _on_maze_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		call_deferred("change_scene_2")
--- scenes/mining/stone.gd
+++ scenes/mining/stone.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-func _on_body_entered(body):
+func _on_stone_body_entered(body):
 	if body.name == "Mining_Player":
 		print("Stone collected")
 		get_tree().call_group("game", "add_stone", 1)
